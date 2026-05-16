2cfd89d89191e5d37eeb94f917bab496883b0f7f
lokeshnathyadav-ctrl <lokeshnath.yadav@gmail.com>
2026-05-17 01:55:36 +0530

    connection to sql-corrected

--- food_delivery_chatbot/prep.py
+++ food_delivery_chatbot/prep.py
@@ -11,13 +11,12 @@ ORDERS_PATH = "hf://datasets/Lokeshnathy/foodhub-orders-data/orders_table.csv"
 CONSUMERS_PATH = "hf://datasets/Lokeshnathy/foodhub-orders-data/consumers_table.csv"
 DELIVERY_PATH = "hf://datasets/Lokeshnathy/foodhub-orders-data/delivery_table.csv"
 print("Dataset loaded successfully")
-
+# Establishing a connection with the provided database.
+connection = sqlite3.connect("DATABASE_PATH")
 # Appending the data with the provided 'customer_orders' database
 orders_df = pd.read_csv(ORDERS_PATH)
 consumers_df = pd.read_csv(CONSUMERS_PATH)
 delivery_df = pd.read_csv(DELIVERY_PATH)
-orders_df.to_sql('orders_table',connection,if_exists='append',index=False)
-consumers_df.to_sql('consumers_table',connection,if_exists='append',index=False)
-delivery_df.to_sql('delivery_table',connection,if_exists='append',index=False)
-# Establishing a connection with the provided database.
-connection = sqlite3.connect("DATABASE_PATH")
+orders_df.to_sql('ORDERS_PATH',connection,if_exists='append',index=False)
+consumers_df.to_sql('CONSUMERS_PATH',connection,if_exists='append',index=False)
+delivery_df.to_sql('DELIVERY_PATH',connection,if_exists='append',index=False)
