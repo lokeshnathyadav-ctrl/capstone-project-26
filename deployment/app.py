5a1a2009f1c23a500100134befc336fd9f8e962b
lokeshnathyadav-ctrl <lokeshnath.yadav@gmail.com>
2026-06-19 11:01:16 +0530

    database path realigned

--- deployment/app.py
+++ deployment/app.py
@@ -37,7 +37,7 @@ ORDERS_PATH = "hf://datasets/Lokeshnathy/foodhub-orders-data/orders_table.csv"
 CONSUMERS_PATH = "hf://datasets/Lokeshnathy/foodhub-orders-data/consumers_table.csv"
 DELIVERY_PATH = "hf://datasets/Lokeshnathy/foodhub-orders-data/delivery_table.csv"
 # Establishing a connection with the provided database.
-connection = sqlite3.connect("DATABASE_PATH")
+connection = sqlite3.connect(DATABASE_PATH)
 # Appending the data with the provided 'customer_orders' database
 orders_df = pd.read_csv(ORDERS_PATH)
 consumers_df = pd.read_csv(CONSUMERS_PATH)
@@ -45,7 +45,7 @@ delivery_df = pd.read_csv(DELIVERY_PATH)
 orders_df.to_sql('ORDERS_PATH',connection,if_exists='append',index=False)
 consumers_df.to_sql('CONSUMERS_PATH',connection,if_exists='append',index=False)
 delivery_df.to_sql('DELIVERY_PATH',connection,if_exists='append',index=False)
-cstmr_db = SQLDatabase.from_uri("sqlite:///customer_orders.db")
+cstmr_db = SQLDatabase.from_uri("sqlite:///customers_orders.db")
 
 db_agent = create_sql_agent(
     llm,
