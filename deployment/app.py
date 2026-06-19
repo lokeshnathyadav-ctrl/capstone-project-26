21c54b1037a5ec5ad797147c4cbd219fd61d05cc
lokeshnathyadav-ctrl <lokeshnath.yadav@gmail.com>
2026-06-19 12:05:10 +0530

    defined database paths in app.py file

--- deployment/app.py
+++ deployment/app.py
@@ -32,12 +32,12 @@ llm = ChatGroq(
 
 api = HfApi(token=os.getenv("HF_TOKEN"))
 # Defining the path of the uploaded data in Hugging Face Hub
-DATABASE_PATH = "hf://datasets/Lokeshnathy/foodhub-orders-data/customer_orders.db"
+#DATABASE_PATH = "hf://datasets/Lokeshnathy/foodhub-orders-data/customer_orders.db"
 ORDERS_PATH = "hf://datasets/Lokeshnathy/foodhub-orders-data/orders_table.csv"
 CONSUMERS_PATH = "hf://datasets/Lokeshnathy/foodhub-orders-data/consumers_table.csv"
 DELIVERY_PATH = "hf://datasets/Lokeshnathy/foodhub-orders-data/delivery_table.csv"
 # Establishing a connection with the provided database.
-connection = sqlite3.connect("customer_orders.db")
+connection = sqlite3.connect("hf://datasets/Lokeshnathy/foodhub-orders-data/customer_orders.db")
 # Appending the data with the provided 'customer_orders' database
 orders_df = pd.read_csv(ORDERS_PATH)
 consumers_df = pd.read_csv(CONSUMERS_PATH)
@@ -45,7 +45,7 @@ delivery_df = pd.read_csv(DELIVERY_PATH)
 orders_df.to_sql('ORDERS_PATH',connection,if_exists='append',index=False)
 consumers_df.to_sql('CONSUMERS_PATH',connection,if_exists='append',index=False)
 delivery_df.to_sql('DELIVERY_PATH',connection,if_exists='append',index=False)
-cstmr_db = SQLDatabase.from_uri("sqlite:///customer_orders.db")
+cstmr_db = SQLDatabase.from_uri("sqlite:///hf://datasets/Lokeshnathy/foodhub-orders-data/customer_orders.db")
 
 db_agent = create_sql_agent(
     llm,
