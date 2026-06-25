5d74d6af7a8e434e5f48b05bd11872d87576a4fe
lokeshnathyadav-ctrl <lokeshnath.yadav@gmail.com>
2026-06-25 12:16:14 +0530

    cloning git repo on streamlit

--- deployment/app.py
+++ deployment/app.py
@@ -18,54 +18,60 @@ from langchain_core.messages import SystemMessage, HumanMessage
 from langchain.memory import ConversationBufferMemory
 from pydantic import BaseModel, Field, ValidationError
 from typing import List, Optional, Dict
+import subprocess
+import sys
+
+#GROQ_API_KEY = os.getenv("GROQ_API_KEY")
+llm = ChatGroq(
+    model = "meta-llama/llama-4-scout-17b-16e-instruct",           # Name of the chat model
+    temperature = 0,                                               # Temperature setting to '0', for consistent and deterministic responses
+    max_tokens = 512,                                             # maximum number of tokens in the output
+    max_retries=2,
+    timeout=None)
+
+def clone_repository(repo_url, local_path):
+    try:
+        subprocess.run(["git", "clone", repo_url, local_path], check=True)
+    except subprocess.CalledProcessError as e:
+        print(f"Failed to clone repository: {e}")
+repo_url = "https://github.com/lokeshnathyadav-ctrl/capstone-project-26.git"
+local_path = "./capstone-project-26"
+if not os.path.exists(local_path):
+    clone_repository(repo_url, local_path)
+sys.path.insert(0, local_path)
+#import my_functions
+#st.write(my_functions.my_function())
 from llm import llm
 from db_tool import db_agent
 from tools import order_query, order_query_tool, answer_query, answer_query_tool
 from chat_agent import ChatBot, chat_agent
-
 # Streamlit UI
 st.title("🍔 FoodHub Delivery ChatBot")
 st.write("Welcome to FoodHub Chat Support Assistant!")
-
-# Session State Initialization
 if "bot" not in st.session_state:
     st.session_state.bot = Chatbot()
-
 if "messages" not in st.session_state:
     st.session_state.messages = []
-
-# Display Previous Messages
-for message in st.session_state.messages:
+for message in st.session_state.messages:   # Display previous messages
     with st.chat_message(message["role"]):
         st.markdown(message["content"])
-
-# User Input
-user_query = st.chat_input("Type your message here...")
-
-# Chat Processing
-if user_query:
-
-    # Show User Message
-    st.session_state.messages.append(
+user_query = st.chat_input("Type your message here...")    # User Input
+if user_query:              # Chat Processing    
+    st.session_state.messages.append(                                     # Chat Processing
         {
             "role": "user",
             "content": user_query
         }
     )
-
     with st.chat_message("user"):
-        st.markdown(user_query)
-
-    # Generate Bot Response
-    response = st.session_state.bot.chat(user_query)   
+        st.markdown(user_query)   
+    response = st.session_state.bot.chat(user_query)    # Generate Bot Response
     # Store Assistant Response
     st.session_state.messages.append(
         {
             "role": "assistant",
             "content": response
         }
-    )
-
-    # Display Assistant Response
-    with st.chat_message("assistant"):
+    )   
+    with st.chat_message("assistant"):                      # Display Assistant Response
         st.markdown(response)
