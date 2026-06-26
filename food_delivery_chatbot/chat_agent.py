8ed832fd14f9d756e65c593f242f01780c4800ca
lokeshnathyadav-ctrl <lokeshnath.yadav@gmail.com>
2026-06-26 22:15:50 +0530

    importing tool module from lchain.tools framework

--- food_delivery_chatbot/chat_agent.py
+++ food_delivery_chatbot/chat_agent.py
@@ -11,7 +11,7 @@ from langchain.agents import create_agent
 from langchain.tools import tool
 from langchain_core.utils.uuid import uuid7
 from langgraph.checkpoint.memory import InMemorySaver
-#from langchain_community.agent_toolkits.load_tools import load_tools
+from langchain_community.agent_toolkits.load_tools import load_tools
 #from langchain.memory import ConversationBufferMemory
 from langchain_core.messages import SystemMessage, HumanMessage
 from pydantic import BaseModel, Field, ValidationError
--- food_delivery_chatbot/queryfunc.py
+++ food_delivery_chatbot/queryfunc.py
@@ -8,7 +8,7 @@ import spacy
 #from langchain_community.agent_toolkits.sql.base import create_sql_agent
 #from langchain_community.agent_toolkits.sql.toolkit import SQLDatabaseToolkit
 #from langchain.agents.agent_types import AgentType
-#from langchain.agents import initialize_agent, Tool
+from langchain.tools import tool
 #from langchain_community.agent_toolkits.load_tools import load_tools
 #from langchain.memory import ConversationBufferMemory
 from langchain_core.messages import SystemMessage, HumanMessage
